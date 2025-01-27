5e8d4a03ab0bff5f4a90e8291478bc6ce9f2e1fb
4pesetas <126103894+4pesetas@users.noreply.github.com>
2025-01-27 13:31:28 +0100

    Add files via upload

---
+++ operations/swap.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iumorave <iumorave@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 11:51:17 by iumorave          #+#    #+#             */
+/*   Updated: 2025/01/27 13:30:24 by iumorave         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+static void	swap(t_stack_node **head)
+{
+	if (!*head || (*head)->next)
+		return ;
+	*head = (*head)->next;
+	(*head)->prev->prev = *head;
+	(*head)->prev->next = (*head)->next;
+	if ((*head)->next)
+		(*head)->next->prev = (*head)->prev;
+	(*head)->next = (*head)->prev;
+	(*head)->prev = NULL;
+}
+
+void	sa(t_stack_node **a, bool print)
+{
+	swap(a);
+	if (!print)
+		ft_printf("sa\n");
+}
+void	sb(t_stack_node **b, bool print)
+{
+	swap(b);
+	if (!print)
+		ft_printf("sb\n");
+}
+
+void	ss(t_stack_node **a, t_stack_node **b, bool print)
+{
+	swap(a);
+	swap(b);
+	if(!print)
+		ft_printf("ss\n");
+}
